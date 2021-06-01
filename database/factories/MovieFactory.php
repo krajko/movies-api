9eb13a41328a0cbb786020bd1e1c811bf2705606
krajko <nikolakrajinovic99@gmail.com>
2021-06-01 18:54:05 +0200

    zadatak 1

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Movie;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MovieFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Movie::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->words(rand(1, 5), true),
+            'director' => $this->faker->name(),
+            'imageUrl' => $this->faker->url(),
+            'duration' => $this->faker->numberBetween(75, 200),
+            'releaseDate' => $this->faker->date(),
+            'genre' =>  $this->faker->word()
+        ];
+    }
+}
